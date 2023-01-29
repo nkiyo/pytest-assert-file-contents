fa3fc8d3cba40df9db2ae131cc3d07b69bef9f69
nkiyo <nakahara.kiyoshi@gmail.com>
2023-01-29 17:16:45 +0900

    init commit

---
+++ sample.py
@@ -0,0 +1,28 @@
+# sudo apt install -y python3-pip
+# pip3 install pytest
+# python3 -m pytest sample.py
+#
+# 適当な関数
+def my_function(s):
+    return s
+
+# fileっぽい内容を文字列定義する
+#file_contents = """\
+#<hoge>
+#  hogehoge
+#</hoge>"""
+
+# fileを文字列として読み込む、関数に渡す
+#my_function(file_contents)
+
+# ユニットテストを実行する
+def test_my_function():
+    # fileっぽい内容を文字列定義する
+    # TODO dedentでインデント削除
+    file_contents = """\
+    <hoge>
+      hogehoge
+    </hoge>"""
+    print(file_contents)
+    actual = my_function(file_contents)
+    assert actual == file_contents
